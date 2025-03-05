d284f7cf850d44c3271cb2666257651d7a942f4f
Oliver Cullen <72854890+The-Mooncake@users.noreply.github.com>
2025-03-05 12:51:03 +0000

    Added device setup. Started on swap chain and back buffers.

--- DXRenderer/MainWindow.cpp
+++ DXRenderer/MainWindow.cpp
@@ -15,10 +15,13 @@
 #include <dxgi1_3.h>
 #include <vector>
 
-#pragma comment(lib, "dxgi") // For CreateDXGIFactory2 linker error.
+// Define SDK version.
+// Requires the Microsoft.Direct3D.D3D12 package (from nuget), version is the middle number of the version: '1.615.1'
+extern "C" { __declspec(dllexport) extern const UINT D3D12SDKVersion = 615; } 
+extern "C" { __declspec(dllexport) extern const char8_t* D3D12SDKPath = u8".\\D3D12\\"; }
 
-// Namespaces
-using namespace Microsoft::WRL;
+// Namespaces and Imports.
+using Microsoft::WRL::ComPtr;
 using namespace DirectX;
 
 // Useful documentation examples:
@@ -26,34 +29,14 @@ using namespace DirectX;
 // https://learn.microsoft.com/en-us/windows/win32/direct3dgetstarted/work-with-dxgi
 // https://learn.microsoft.com/en-us/windows/win32/direct3d12/directx-12-programming-guide
 // https://www.rastertek.com/tutdx11win10.html
+// https://walbourn.github.io/anatomy-of-direct3d-12-create-device/
 
 
 MainWindow::MainWindow(HINSTANCE InHInstance)
 {
     hInstance = InHInstance;
 
-
-    UINT dxgiFactoryFlags = 0;
-    ComPtr<IDXGIFactory7> Factory;
-    CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&Factory));
-
-    
-    UINT i = 0;
-    ComPtr<IDXGIAdapter1> Adapter;
-    DXGI_ADAPTER_DESC1 AdapterDesc;
-    while (Factory->EnumAdapters1(i, &Adapter) != DXGI_ERROR_NOT_FOUND)
-    {
-        Adapter.Get()->GetDesc1(&AdapterDesc);
-        if (AdapterDesc.VendorId == 4318) // NVIDIA Vendor ID !VERY HACKY! 
-        {
-            break;
-        }
-        ++i;
-    }
-
-    ComPtr<ID3D12Device14> Device;
-    //HRESULT hr = D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_2, IID_PPV_ARGS(&Device));
-    
+    SetupDX();
 }
 
 LRESULT CALLBACK MainWindow::WinProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -65,6 +48,9 @@ LRESULT CALLBACK MainWindow::WinProcedure(HWND hWnd, UINT message, WPARAM wParam
     switch (message)
     {
     case WM_CLOSE:
+        DestroyWindow(hWnd);
+        break;
+        
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -92,9 +78,120 @@ LRESULT CALLBACK MainWindow::WinProcedure(HWND hWnd, UINT message, WPARAM wParam
     return 0;
 }
 
+void MainWindow::SetupDX()
+{
+    HRESULT hr;
+
+    // Setup DX Factory
+    hr = CreateDXGIFactory1(IID_PPV_ARGS(&Factory));
+    if (FAILED(hr))
+    {
+        printf("Failed to create D3D12 Factory\n");
+        PostQuitMessage(1);
+        return;
+    }
+
+    // Get Adapter
+    DXGI_ADAPTER_DESC1 AdapterDesc;
+    while (Factory->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&Adapter)) != DXGI_ERROR_NOT_FOUND)
+    {
+        hr = Adapter.Get()->GetDesc1(&AdapterDesc);
+        break;
+    }
+    if (FAILED(hr))
+    {
+        printf("Failed to create D3D12 Factory\n");
+        PostQuitMessage(1);
+        return;
+    }
+
+    // Get the device.
+    hr = D3D12CreateDevice(Adapter.Get(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&Device));
+    if (FAILED(hr))
+    {
+        printf("Failed to create D3D12 device\n");
+        PostQuitMessage(1);
+        return;
+    }
+
+    // GFX Pipeline or Context in DX11.
+    // D3D12_GRAPHICS_PIPELINE_STATE_DESC Desc{};
+    // hr = Device->CreateGraphicsPipelineState(&Desc ,IID_PPV_ARGS(&Device));
+    // if (FAILED(hr))
+    // {
+    //     printf("Failed to create graphics pipeline\n");
+    //     PostQuitMessage(1);
+    //     return;
+    // }
+    
+    D3D12_COMMAND_QUEUE_DESC cmdQueueDesc{};
+    hr = Device->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&CmdQueue));
+    if (FAILED(hr))
+    {
+        printf("Failed to create command queue\n");
+        PostQuitMessage(1);
+        return;
+    }
+    
+    // DX Setup correctly.
+    bDXReady = true;
+}
+
+void MainWindow::SetupSwapChain()
+{
+    HRESULT HR;
+    
+    DXGI_SWAP_CHAIN_DESC1 desc;
+    ZeroMemory(&desc, sizeof(DXGI_SWAP_CHAIN_DESC1));
+    desc.BufferCount = 2;
+    desc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    desc.SampleDesc.Count = 1;      //multisampling setting
+    desc.SampleDesc.Quality = 0;    //vendor-specific flag
+    desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+    
+
+     // Might need to use CreateSwapChainForCoreWindow || CreateSwapchainForComposition...
+     HR = Factory->CreateSwapChainForHwnd(CmdQueue.Get(), hWnd, &desc, nullptr, nullptr, &SwapChain);
+     if (FAILED(HR))
+     {
+         printf("Failed to create swap chain.\n");
+         PostQuitMessage(1);
+         return;
+     }
+
+    // RT Heaps
+    D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+    rtvHeapDesc.NumDescriptors = FrameCount;
+    rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    HR = Device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&RtvHeap));
+    if (FAILED(HR))
+    {
+        printf("Failed to create rtv heap.\n");
+        PostQuitMessage(1);
+        return;
+    }
+    
+    D3D12_CPU_DESCRIPTOR_HANDLE BufferHandle(RtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+    //Device->CreateRenderTargetView()
+    
+    D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+    rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+    Device->CreateRenderTargetView(BackBuffer.Get(), &rtvDesc, BufferHandle);
+    BufferHandle.ptr += sizeof(D3D12_CPU_DESCRIPTOR_HANDLE); // Offset by one. // Likely this is where its failing as the buffer is not being offset or assigned correctly. 
+    //Device->CreateRenderTargetView(FrontBuffer.Get(), &rtvDesc, BufferHandle);
+
+    
+}
+
 
 int MainWindow::Run()
 {
+    if (!bDXReady) { return 1; }
+    
     static TCHAR szWindowClass[] = _T("DXRenderer");
     static TCHAR szTitle[] = _T("Win D3D Renderer - Mooncake");
     
@@ -152,16 +249,18 @@ int MainWindow::Run()
     // Show, its hidden by default.
     ShowWindow(hWnd, SW_SHOW);
     UpdateWindow(hWnd);
+
+    // Start DX.
+    SetupSwapChain();
     
     // Message loop
     MSG msg;
-    bool bMsg = false;
     msg.message = WM_NULL;
-    PeekMessage(&msg, hWnd, 0, 0, PM_NOREMOVE);
+    PeekMessage(&msg, nullptr, 0, 0, PM_NOREMOVE);
     
     while (msg.message != WM_QUIT)
     {
-        bMsg = (PeekMessage(&msg, hWnd, 0, 0, PM_REMOVE) != 0);
+        bool bMsg = (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE) != 0);
     
         if (bMsg)
         {
@@ -171,8 +270,7 @@ int MainWindow::Run()
         else
         {
             // Update renderer.
-            
-            
+
         }
     
     }
--- DXRenderer/MainWindow.h
+++ DXRenderer/MainWindow.h
@@ -1,9 +1,17 @@
 #pragma once
 
 #include <windows.h>
+#include <wrl/client.h>
+
+// DX
+#include <d3dcommon.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
 
 // Global ptr to the main window instance.
 //static class MainWindow* G_MainWindow = nullptr;
+
+using  Microsoft::WRL::ComPtr;
 
 class MainWindow
 {
@@ -15,6 +23,25 @@ public:
     static LRESULT CALLBACK WinProcedure(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 private:
+
+    void SetupDX();
+    void SetupSwapChain();
+    
+    bool bDXReady = false;
+
+    // DX resources.
+    ComPtr<IDXGIFactory7> Factory;
+    ComPtr<IDXGIAdapter1> Adapter;
+    ComPtr<ID3D12Device7> Device;
+    ComPtr<ID3D12CommandQueue> CmdQueue;
+    ComPtr<IDXGISwapChain1> SwapChain;
+
+    // Buffers
+    ComPtr<ID3D12DescriptorHeap> RtvHeap;
+    UINT FrameCount = 2; // Currently only two buffers
+    ComPtr<ID3D12Resource> BackBuffer;
+    ComPtr<ID3D12Resource> FrontBuffer;
+    
     HINSTANCE hInstance = nullptr;
     HWND hWnd = nullptr;
 };
