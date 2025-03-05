2f52432c080271a7fcd69665f85918c58fccca37
Oliver Cullen <72854890+The-Mooncake@users.noreply.github.com>
2025-03-05 18:11:26 +0000

    Exclude HLSL shaders from compiling by setting their type to 'text', do copy to output dir.

--- DXRenderer/Shaders.hlsl
+++ DXRenderer/Shaders.hlsl
@@ -1,26 +1,26 @@
-// #pragma once
-//
-// struct VS_INPUT
-// {
-//     float3 Position : POSITION;
-//     float3 Colour : COLOR;
-// };
-//
-// struct VS_OUTPUT
-// {
-//     float4 Position : SV_POSITION;
-//     float3 Colour : COLOR;
-// };
-//
-// VS_OUTPUT VSMain(VS_INPUT In)
-// {
-//     VS_OUTPUT Out;
-//     Out.Position = float4(In.Position, 0.0f);
-//     Out.Colour = float3(0.65f, 0.65f, 0.65f);
-//     return Out;
-// }
-//
-// float4 PSMain(VS_OUTPUT In) : SV_TARGET
-// {
-//     return float4(In.Colour, 1.0f);
-// }
+#pragma once
+
+struct VS_INPUT
+{
+    float3 Position : POSITION;
+    float3 Colour : COLOR;
+};
+
+struct VS_OUTPUT
+{
+    float4 Position : SV_POSITION;
+    float3 Colour : COLOR;
+};
+
+VS_OUTPUT VSMain(VS_INPUT In)
+{
+    VS_OUTPUT Out;
+    Out.Position = float4(In.Position, 0.0f);
+    Out.Colour = float3(0.65f, 0.65f, 0.65f);
+    return Out;
+}
+
+float4 PSMain(VS_OUTPUT In) : SV_TARGET
+{
+    return float4(In.Colour, 1.0f);
+}
