66981ae80607a3dfaa7e968f873983145f7705cc
The-Mooncake <72854890+The-Mooncake@users.noreply.github.com>
2025-03-23 20:39:58 +0000

    CMake with working USD, no source vcpkg version.

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -8,8 +8,8 @@ CALL "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevC
 cd
 
 cmake .. -G "Visual Studio 17 2022" -A "x64" ^
- -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
- -DCMAKE_PREFIX_PATH="/Project/vcpkg_installed/x64-windows/share/directx-headers,/Project/vcpkg_installed/x64-windows/share/directx12-agility"
+-DCMAKE_TOOLCHAIN_FILE"=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake" ^
+-DCMAKE_PREFIX_PATH="\Project\vcpkg_installed\x64-windows\share\directx-headers,\Project\vcpkg_installed\x64-windows\share\directx12-agility"
 
 
 pause
--- Src/DXRenderer.cpp
+++ Src/DXRenderer.cpp
@@ -5,11 +5,19 @@
 // Renderer
 #include "MainWindow.h"
 
+// USD
+#include <pxr/usd/usd/stage.h>
+
+
 // Force Nvidia GPU - e.g in laptop cases.
 extern "C" { __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001; }
 
 int WINAPI WinMain( _In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+    pxr::UsdStageRefPtr stage = pxr::UsdStage::CreateInMemory();
+    
+
+
     MainWindow App = MainWindow(hInstance);
     const int ExitCode = App.Run();
 
